ca7bae449c8155245fcc484862b97efe6c3241c5
Cesar <c.orozco1293@gmail.com>
2018-09-20 19:46:03 -0500

    Reporte 3 terminado

--- app/src/main/java/com/movil/clase/tallercelulares/Reporte.java
+++ app/src/main/java/com/movil/clase/tallercelulares/Reporte.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class Reporte extends AppCompatActivity {
 
     private ListView lv;
@@ -33,6 +35,7 @@ public class Reporte extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 switch (i){
                     case 0:
+                        reporte3();
                         break;
                     case 1:
                         break;
@@ -45,21 +48,43 @@ public class Reporte extends AppCompatActivity {
 
     public void reporte3(){
         ArrayList<Celular> celulares;
-        ArrayList<Celular> huawei;
-        huawei = new ArrayList<>();
+        ArrayList<Celular> huawei = new ArrayList<>();
         int menor, i=0;
+        String marca = "", color ="", so = "";
         celulares = Datos.obtener();
-        while (celulares.size() >= i){
-            if (celulares.get(i).getMarca() == getResources().getString(R.string.huawei)
-                    && celulares.get(i).getColor() == getResources().getString(R.string.blanco)
-                    && celulares.get(i).getSo() == getResources().getString(R.string.android)){
-                huawei.add(celulares.get(i));
-            }
-        }
-        if (huawei.isEmpty()){
+        if (celulares.isEmpty()) {
             Toast.makeText(this,
-                    getResources().getString(R.string.mensaje_reporte_tres),
-                    Toast.LENGTH_SHORT).show();
+                    getResources().getString(R.string.no_agregados),
+                    LENGTH_SHORT).show();
+        } else {
+            while (celulares.size() > i) {
+                if (celulares.get(i).getMarca().equalsIgnoreCase(getResources().getString(R.string.huawei))
+                        && celulares.get(i).getColor().equalsIgnoreCase(getResources().getString(R.string.blanco))
+                        && celulares.get(i).getSo().equalsIgnoreCase(getResources().getString(R.string.android))) {
+                    huawei.add(celulares.get(i));
+                }
+                i++;
+            }
+            if (huawei.isEmpty()) {
+                Toast.makeText(this,
+                        getResources().getString(R.string.mensaje_reporte_tres),
+                        LENGTH_SHORT).show();
+            } else {
+                menor = huawei.get(0).getPrecio();
+                for (int j = 0; j < huawei.size(); j++) {
+                    if (huawei.get(j).getPrecio() <= menor) {
+                        menor = huawei.get(j).getPrecio();
+                        marca = huawei.get(j).getMarca();
+                        color = huawei.get(j).getColor();
+                        so = huawei.get(j).getSo();
+                    }
+                }
+                Toast.makeText(this, getResources().getString(R.string.precio)
+                        + ": " + menor + " " + getResources().getString(R.string.marca)
+                        + ": " + marca + " " + getResources().getString(R.string.color)
+                        + ": " + color + " " + getResources().getString(R.string.sistemaOperativo)
+                        + ": " + so, LENGTH_SHORT).show();
+            }
         }
     }
 
